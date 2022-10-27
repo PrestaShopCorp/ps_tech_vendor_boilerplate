40e95cce6c2febd5b3bd7763d5030f1736c0d477
Mikatux <mickael@mayeur.eu>
2022-10-27 15:30:41 +0200

    First commit

---
+++ ps_tech_vendor_boilerplate.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * 2007-2022 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2022 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+
+use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Ps_tech_vendor_boilerplate extends Module
+{
+    /**
+     * @var \PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer
+     */
+    private $serviceContainer;
+    public function __construct()
+    {
+        $this->name = 'ps_tech_vendor_boilerplate';
+        $this->tab = 'content_management';
+        $this->version = '1.0.0';
+        $this->author = 'Mika';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Cloudsync module template');
+        $this->description = $this->l('This is a template module for Cloudsync');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+
+        $this->serviceContainer = new \PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer(
+            $this->name,
+            $this->getLocalPath()
+        );
+    }
+
+    public function install()
+    {
+        $moduleManager = ModuleManagerBuilder::getInstance()->build();
+
+        if (!$moduleManager->isInstalled("ps_eventbus")) {
+            $moduleManager->install("ps_eventbus");
+        } else if (!$moduleManager->isEnabled("ps_eventbus")) {
+            $moduleManager->enable("ps_eventbus");
+        }
+        $moduleManager->upgrade('ps_eventbus');
+
+
+
+        /*
+        $eventbusModule =  \Module::getInstanceByName("ps_eventbus");
+        $eventbusPresenterService = $eventbusModule->getService('PrestaShop\Module\PsEventbus\Service\PresenterService');
+        $eventbusPresenterService->init();
+*/
+        return parent::install();
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
+
+        $moduleManager = ModuleManagerBuilder::getInstance()->build();
+        /*
+        if ($moduleManager->isInstalled("ps_accounts")) {
+
+            $accountsModule =  \Module::getInstanceByName("ps_accounts");
+            $accountPresenterService = $accountsModule->getService('PrestaShop\Module\PsAccounts\Presenter\PsAccountsPresenter');
+
+            Media::addJsDef([
+                'contextPsAccounts' => $accountPresenterService->present($this->name),
+            ]);
+        }
+        */
+        if ($moduleManager->isInstalled("ps_eventbus")) {
+            $eventbusModule =  \Module::getInstanceByName("ps_eventbus");
+            $eventbusPresenterService = $eventbusModule->getService('PrestaShop\Module\PsEventbus\Service\PresenterService');
+
+            Media::addJsDef([
+                'contextPsEventbus' => $eventbusPresenterService->expose($this->name, ['order', 'product'], ['customer'])
+            ]);
+        } else {
+            Media::addJsDef([
+                'contextPsEventbus' => []
+            ]);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array();
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array();
+    }
+}
---
+++ views/templates/admin/configure.tpl
@@ -0,0 +1,54 @@
+{*
+* 2007-2022 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2022 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*}
+
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/zoid/9.0.87/zoid.min.js"
+    integrity="sha512-PqylMx5T7MS4lZRe4qziZWQ24VWWSF3rNiNJQuswCJHJb+HDW6aQvrTFrIrF+kPl0IS2eFf/ZkFNysjsahEahg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  ></script>
+  <script src="https://integration-assets.prestashop3.com/ext/cloudsync-merchant-sync-consent/latest/cloudsync-cdc.js"></script>
+<div class="panel">
+	<h3><i class="icon icon-credit-card"></i> Cloudsync module example</h3>
+	<p>
+		<strong>Here is my new generic module!</strong><br />
+		Thanks to PrestaShop, now I have a great module.<br />
+		I can configure it using the following configuration form.
+	</p>
+	<br />
+	<p>
+		This module will boost your sales!
+	</p>
+</div>
+
+<div id="ps_eventbus_cdc"></div>
+<script>
+    ShareConsentBar({
+        context: window.contextPsEventbus,
+        onConsentValidate: () => {
+            console.log('User validate the consents');
+        },
+    }).render('#ps_eventbus_cdc');
+</script>
